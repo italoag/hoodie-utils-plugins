0ab5bf3a82bf7227b2c89db7f403cc247dc7dd98
Gabriel Mancini <gabriel.mancini@gmail.com>
2014-11-14 17:50:17 -0200

    add replicatior

--- index.js
+++ index.js
@@ -195,3 +195,38 @@ utils.ExtendedDatabaseAPI = function ExtendedDatabaseAPI(hoodie, db) {
 
   return db;
 };
+
+utils.ReplicatorAPI = function ReplicatorAPI (hoodie) {
+  var Replicator = this;
+
+  Replicator.find = function (type, id, callback) {
+    var replicatorId = encodeURIComponent([type, id].join('/'));
+    hoodie.request('GET', '/_replicator/' + replicatorId, {}, callback);
+  };
+
+  Replicator.add = function (type, id, data, callback) {
+    Replicator.find(type, id, function (err, _doc, res) {
+      if (res.statusCode === 404) {
+        return hoodie.request('POST', '/_replicator', { data: data }, callback);
+      } else if (err) {
+        return callback(err);
+      }
+      return callback('Replicator already exists.', _doc);
+    });
+  };
+
+  Replicator.remove = function (type, id, callback) {
+    var replicatorId = encodeURIComponent([type, id].join('/'));
+    Replicator.find(type, id, function (err, _doc, res) {
+      if (res.statusCode === 404) {
+        return callback('Replicator not found.');
+      } else if (err) {
+        return callback(err);
+      }
+
+      hoodie.request('DELETE', '/_replicator/' + replicatorId + '?rev=' + _doc._rev, {}, callback);
+    });
+  };
+
+  return Replicator;
+};
